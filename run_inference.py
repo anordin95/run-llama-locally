1ef09c7cd5d5449c55ecade5169dd27a8b8c426b
anordin95 <alexander.f.nordin@gmail.com>
2024-10-01 12:15:23 -0700

    Cleanup the beam-search implementation.

--- run_inference.py
+++ run_inference.py
@@ -23,7 +23,7 @@ print(f"Using model: {MODEL_NAME}.")
 LLAMA_MODELS_DIR = Path.home() / ".llama" / "checkpoints"
 
 # This is the prompt/input you'd like to pass to the model.
-INPUT_STRING = "Hi. Tell me about yoruself. Who are you? What do you do?"
+INPUT_STRING = "Hi. Who are you? What do you do? Please tell me about yourself. Do try to be concise."
 
 # mps stands for Metal Performance Shaders, i.e. Apple GPU's.
 # Something went awry when I tried using mps; the model output a tensor 
@@ -43,6 +43,7 @@ torch.distributed.init_process_group(world_size=1, rank=0, store=torch.distribut
 
 # Initialize the fairscale package.
 initialize_model_parallel(model_parallel_size_=1)
+print()
 
 # ==========================================================================
 # Load the model & model-weights.
@@ -75,13 +76,16 @@ tokenizer = Tokenizer(model_path=str(token_dictionary_path))
 print(f"Converting input-string: '{INPUT_STRING}' to tokens.")
 input_tokens = tokenizer.encode(s=INPUT_STRING, bos=True, eos=True)
 # The model expects int64's (i.e. LongTensor). The extra [] are there to add a batch-dimension.
-input_batch = torch.LongTensor([input_tokens + [128000]]).to(DEVICE)
+# Also, automatically add the beginning-of-sequence token.
+beginning_of_sequence_token = 128_000
+input_batch = torch.LongTensor([input_tokens + [beginning_of_sequence_token]]).to(DEVICE)
 
 # ==========================================================================
 # Run inference.
 
-class PossibleTokenSequence:
+class TokenSequenceAndProbability:
     def __init__(self, starting_sequence: torch.LongTensor):
+        # Starting sequence is expected to have shape (1, N) where N is the sequence-length.
         self.token_sequence = starting_sequence
         self.log_p = 0.0
         self.initial_sequence_length = starting_sequence.shape[-1]
@@ -95,28 +99,31 @@ class PossibleTokenSequence:
     def get_output_sequence(self):
         return self.token_sequence[:, self.initial_sequence_length:]
 
+    def get_output_sequence_length(self):
+        return self.get_output_sequence().shape[-1]
+
 beam_width = 7
-possible_token_sequences = [PossibleTokenSequence(input_batch) for _ in range(beam_width)]
+possible_sequences_and_probs = [TokenSequenceAndProbability(input_batch) for _ in range(beam_width)]
 
-next_most_likely_token = None
-end_of_sequence_token = 128001
+end_of_sequence_token = 128_001
 max_seq_len = 256
-is_first_inference_pass = True
 
-while next_most_likely_token != end_of_sequence_token:
-        
-    current_seq_len = possible_token_sequences[0].get_output_sequence().shape[-1]
-    if current_seq_len >= max_seq_len:
-        print(f"Reached maximum sequence length of: {max_seq_len}.")
-        break
+for beam_idx in range(beam_width):
     
-    for beam_idx in range(beam_width):
+    print(f"\nComputing beam: {beam_idx + 1}'s sequence.")
+    
+    possible_sequence_and_prob = possible_sequences_and_probs[beam_idx]
+    next_most_likely_token = None
+    is_beams_first_inference_pass = True
 
-        possible_token_sequence = possible_token_sequences[beam_idx]
+    while next_most_likely_token != end_of_sequence_token:    
 
-        # What does start_pos mean?
-        output_batch = llama_model(possible_token_sequence.token_sequence, start_pos=0)
-
+        if possible_sequence_and_prob.get_output_sequence_length() >= max_seq_len:
+            print(f"Reached maximum sequence length of: {max_seq_len}.")
+            break
+            
+        output_batch = llama_model(possible_sequence_and_prob.token_sequence, start_pos=0)
+        
         # Take the first batch-output. Recall there was only one batch-input.
         output_activations = output_batch[0]
 
@@ -126,35 +133,35 @@ while next_most_likely_token != end_of_sequence_token:
         # We only care about the final next-token prediction, i.e. the next token
         # given our input-token sequence.
         next_token_activations = output_activations[-1]
-
+        
         # Softmax the outputs, then select the most likely token at each step.
         # The next-word activation vector has the same dimensionality as the 
         # token-vocabulary. Take the softmax over the vector to obtain a probability
         # distribution over possible tokens.
         next_token_probabilities = next_token_activations.softmax(dim=0)
-        
-        if is_first_inference_pass:
+
+        if is_beams_first_inference_pass:
             # On the first pass, we want to initialize the beam-sequences with the most likely {beam-width}
-            # possible tokens.
+            # possible tokens. That is, beam 1 begins with the most likely token, beam 2 begins with
+            # the 2nd most likely token, etc.
             next_most_likely_token = torch.topk(next_token_probabilities, k=beam_width, dim=0).indices[beam_idx]
+            is_beams_first_inference_pass = False
         else:
             # Select the index (i.e. the token) that has been assigned the highest probability value.
             next_most_likely_token = torch.argmax(next_token_probabilities, dim=0)
-
+        
         next_most_likely_token_str = tokenizer.decode(t=[next_most_likely_token])
 
         # Find the probability associated with the token that was chosen.
         most_likely_token_probability = torch.max(next_token_probabilities, dim=0).values.item()
         
         # Keep track of the most-likely tokens. And, add the predicted token to the input.
-        possible_token_sequence.append_token(token=next_most_likely_token, p=most_likely_token_probability)
+        possible_sequence_and_prob.append_token(token=next_most_likely_token, p=most_likely_token_probability)
 
-        print(f"For beam: {beam_idx + 1}, the model thinks token {next_most_likely_token_str} is the most likely token to come next with p: {most_likely_token_probability:.3f}.")
+        print(f"The model thinks token {next_most_likely_token_str} is the most likely token to come next with p: {most_likely_token_probability:.3f}.")
 
-    is_first_inference_pass = False
-
-# Decode the output tokens.
+# Decode each beam's output sequence.
 for beam_idx in range(beam_width):
-    possible_token_sequence = possible_token_sequences[beam_idx]
-    decoded_tokens = tokenizer.decode(t=possible_token_sequence.get_output_sequence().squeeze().tolist())
-    print(f"\n\nBeam: {beam_idx+1} predicted tokens correspond to this string: \n'{decoded_tokens}' with log-p: {possible_token_sequence.log_p:.4f}.")
+    possible_sequence_and_prob = possible_sequences_and_probs[beam_idx]
+    decoded_tokens = tokenizer.decode(t=possible_sequence_and_prob.get_output_sequence().squeeze().tolist())
+    print(f"\n\nBeam: {beam_idx+1} predicted tokens with total log-p: {possible_sequence_and_prob.log_p:.4f} that correspond to this string: \n'{decoded_tokens}'.")
